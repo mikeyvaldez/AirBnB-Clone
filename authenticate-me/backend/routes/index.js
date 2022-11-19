b1f2841aec5c89a051b936d70b904008469ffa7c
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-11-18 21:31:33 -0800

    Add an API router

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
 
+
+router.use('/api', apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
