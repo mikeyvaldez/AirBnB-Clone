8ac767f8c0d0fd5a69e2096956523f2374f5471b
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-11-22 13:20:30 -0800

    udated db

--- backend/db/seeders/20221119055344-demo-user.js
+++ backend/db/seeders/20221119055344-demo-user.js
@@ -35,7 +35,7 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    options.tableName = 'Users';
+    options.tableName = 'Users'
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
