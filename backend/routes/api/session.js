d197cc06de604b835278435c7a71c9f285775674
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-11-24 00:53:16 -0800

    edit login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -17,53 +17,50 @@ const validateLogin = [
 ];
 
 // Log in
-router.post(
-  '/',
-  validateLogin,
-  async (req, res, next) => {
+router.post('/', validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
 
     if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
+      res.status(401);
+      res.json({
+        "message": "invalid credentials",
+        "statusCode": 401
+      })
     }
 
     await setTokenCookie(res, user);
 
-    return res.json({
-      user: user
-    });
-  }
-);
+    let myObj = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      username: user.username,
+      token: ""
+    }
+
+    return res.json(myObj);
+});
 
 
 // Log out
-router.delete(
-  '/',
-  (_req, res) => {
-    res.clearCookie('token');
-    return res.json({ message: 'success' });
-  }
-);
+router.delete( '/',  (_req, res) => {
+  res.clearCookie('token');
+
+  return res.json({ message: 'success' });
+});
 
 // Restore session user
-router.get(
-  '/',
-  restoreUser,
-  (req, res) => {
-    const { user } = req;
-    if (user) {
-      return res.json({
-        user: user.toSafeObject()
-      });
-    } else return res.json({ user: null });
-  }
-);
+router.get( '/', (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject()
+    });
+  } else return res.json(null);
+});
 
 
 
