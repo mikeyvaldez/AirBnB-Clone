36a3d11cee1e9c039e28827228b7d49d8577e826
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-11-26 23:30:28 -0800

    updated seeder

---
+++ backend/db/seeders/20221126050955-test-Spots.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+      await queryInterface.bulkInsert('Spots', [
+        {
+          id: 1,
+          ownerId: 1,
+          address: "123 Disney Lane",
+          city: "San Francisco",
+          state: "California",
+          country: "United States of America",
+          lat: 37.765358,
+          lng: -122.4730327,
+          name: "App Academy",
+          description: "Place where web developers are created",
+          price: 123,
+          createdAt: "2021-11-19 20:39:36",
+          updatedAt: "2021-11-19 20:39:36",
+          avgRating: 4.5,
+          previewImage: "image url"
+        }
+      ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete('Spots', {
+      city: { [Op.is]: 'San Francisco' }
+    }, {});
+  }
+};
