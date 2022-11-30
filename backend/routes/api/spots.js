b59e896c84062dceda46880d25300c1bec103c93
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-11-30 15:11:15 -0800

    spot validation

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,51 @@ const express = require('express');
 const router = express.Router();
 const { Spot, User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
+const {check } = require('express-validator');
 const {Op} = require('sequelize');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+const validateSpot = [
+  check('address')
+    .notEmpty()
+    .withMessage('Street address is required'),
+  check('city')
+    .notEmpty()
+    .withMessage('City is required'),
+  check('state')
+    .notEmpty()
+    .withMessage('State is required'),
+  check('country')
+    .notEmpty()
+    .withMessage('Country is required'),
+  check('lat')
+    .isDecimal()
+    .withMessage('Latitude is not valid'),
+  check('lng')
+    .isDecimal()
+    .withMessage('Longitude is not valid'),
+  check('name')
+    .isLength({max:50})
+    .withMessage('Name must be less than 50 characters'),
+  check('description')
+    .notEmpty()
+    .withMessage('Description is  required'),
+  check('price')
+    .notEmpty()
+    .isInt()
+    .withMessage('Price per day is required'),
+  handleValidationErrors
+]
+
+
+// Create a spot
+router.post('/', validateSpot, requireAuth, async (req, res, next) => {
+  const ownerId = req.user.ownerId
+
+  const newSpot = await Spot.create({ ownerId, ...req.body })
+  res.json(newSpot)
+})
 
 
 // GET all spots
@@ -16,7 +60,6 @@ router.get('/', async (req, res) => {
   res.json(spots)
 })
 
-// GET all spots owned by Current User
 
 
 module.exports = router;
