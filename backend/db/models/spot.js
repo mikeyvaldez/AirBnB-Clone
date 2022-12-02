8c2d3a77a98370969a66483fce7ad9d9faf90ced
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-12-02 09:16:38 -0800

    No migrations made

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
 
     static associate(models) {
       // define association here
-      Spot.belongsTo(models.User, { foreignKey: 'ownerId', as:'Owner' })
+      Spot.belongsTo( models.User, { foreignKey: 'ownerId', as:'Owner' })
     }
   }
   Spot.init({
