e54fa5824bc0ac7dd4ee24490430fdcf29e666ab
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-12-02 09:35:55 -0800

    fixed bug in migrations

--- backend/db/migrations/20221127080107-create-spot.js
+++ backend/db/migrations/20221127080107-create-spot.js
@@ -12,7 +12,7 @@ module.exports = {
       },
       ownerId: {
         type: Sequelize.INTEGER,
-        references: {model: 'User', key: 'id'},
+        references: {model: 'Users', key: 'id'},
         onDelete: 'CASCADE',
         hooks:true
       },
