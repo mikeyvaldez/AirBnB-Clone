12c9dab313dd49c203ca1c57a586b04d7b81a464
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-12-04 21:05:10 -0800

    migrated tables

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Spot, User, SpotImage } = require('../../db/models');
+const { Spot, User, SpotImage, Review, ReviewImage, Booking } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 const {check } = require('express-validator');
 const {Op} = require('sequelize');
