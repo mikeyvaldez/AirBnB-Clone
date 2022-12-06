d02ce3baaa8321bc9ee9cede80280b1094196003
Michael Valdez <mikeyvaldez92@yahoo.com>
2022-12-05 20:15:28 -0800

    get details of a spot by Id

---
+++ backend/db/models/review.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model, DatabaseError
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Review.belongsTo(models.User, { foreignKey: 'userId'});
+      Review.belongsTo(models.Spot, { foreignKey: 'spotId'});
+    }
+  }
+  Review.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    review: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    stars: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        min: 0,
+        max: 5
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
